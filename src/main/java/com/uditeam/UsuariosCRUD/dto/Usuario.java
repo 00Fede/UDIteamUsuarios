3e20502573decd287a15b45b0337e34f509022d3
00Fede <feedkiko@gmail.com>
2017-06-22 15:56:16 -0500

    Inicio del proyecto, creacion de la entidad

---
+++ src/main/java/com/uditeam/UsuariosCRUD/dto/Usuario.java
@@ -0,0 +1,115 @@
+/**
+ * 
+ */
+package com.uditeam.UsuariosCRUD.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Esta clase representa una entidad Usuario de la base de datos
+ * @author Administrator
+ *
+ */
+@Entity
+public class Usuario {
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the apellido
+	 */
+	public String getApellido() {
+		return apellido;
+	}
+	/**
+	 * @param apellido the apellido to set
+	 */
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	/**
+	 * @return the usuario
+	 */
+	public String getUsuario() {
+		return usuario;
+	}
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	/**
+	 * @return the contrasena
+	 */
+	public String getContrasena() {
+		return contrasena;
+	}
+	/**
+	 * @param contrasena the contrasena to set
+	 */
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the estado
+	 */
+	public String getEstado() {
+		return estado;
+	}
+	/**
+	 * @param estado the estado to set
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	@Id
+	int id;
+	@NotNull
+	String nombre;
+	@NotNull
+	String apellido;
+	@NotNull
+	String usuario;
+	@NotNull
+	String contrasena;
+	@NotNull
+	String email;
+	@NotNull
+	String estado;
+}
